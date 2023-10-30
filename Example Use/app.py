64869f712cefc602940e0823a2930f2703b09154
HussBTWYT <121028340+HussBTWYT@users.noreply.github.com>
2023-10-30 21:50:05 +0000

    Update app.py

--- Example Use/app.py
+++ Example Use/app.py
@@ -1,1 +1,10 @@
-print("Hello World!")
+from flask import Flask
+
+app = Flask(__name__)
+
+@app.route("/") # / = The default HOMEPAGE of every single website, in the world (type: decorator)
+def index():
+  return "Hello, World!" # This is converted right into a HTML template, so using specific elements e.g. <h1> would make it a header.
+
+app.run() # Remove if commercial, search for your hostings website documentation on how you can remove this, while keeping the whole project succesfully running.
+  
